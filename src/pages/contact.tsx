1e93dfb6ea0f74b0cc4c728a1c604fac9a92ffab
sMorrison26 <morris10@rpi.edu>
2023-12-31 15:07:33 -0500

    Adding brands to contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,7 +2,8 @@ import Layout from '@/components/layout/layout.component'
 import type { ReactElement } from 'react'
 import type { NextPageWithLayout } from './_app'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFaceAngry } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 
 const Contact: NextPageWithLayout = () => {
@@ -11,13 +12,13 @@ const Contact: NextPageWithLayout = () => {
       <div className="text-xl sm:text-4xl mx-auto mb-4">
         <p>Let&apos;s make something great together!</p>
       </div>
-      <div className=" sm:w-1/2 border border-purple-600 sm:mx-auto">
-        <div className="flex justify-between w-1/2 mx-auto p-4">
-          <FontAwesomeIcon className='' icon={faFaceAngry} />
-          <FontAwesomeIcon className='' icon={faFaceAngry} />
+      <div className="md:text-xl md:w-1/2 border border-purple-600 md:mx-auto">
+        <div className="flex justify-between w-1/2 mx-auto p-4 text-2xl sm:text-3xl">
+          <FontAwesomeIcon className='' icon={faLinkedin} />
+          <FontAwesomeIcon className='' icon={faGithub} />
         </div>
         <hr className='w-4/5 mx-auto'></hr>
-        <div className='flex flex-col sm:flex-row sm:justify-between p-4 overflow-hidden'>
+        <div className='flex flex-col md:flex-row md:justify-between p-4 overflow-hidden'>
           <p><strong>Email: </strong><a className='hover:underline' href='mailto:morrisonsean123@gmail.com'>morrisonsean123@gmail.com</a></p>
           <p><strong>Mobile:&nbsp;</strong><a className='hover:underline' href='tel:4843190679'>(484) 319-0679</a></p>
         </div>
