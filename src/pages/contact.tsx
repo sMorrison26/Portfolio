8e52114df6084f2202d74589d5b5330aeeb378de
sMorrison26 <morris10@rpi.edu>
2023-12-31 15:26:46 -0500

    More styling!

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -8,14 +8,18 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const Contact: NextPageWithLayout = () => {
   return(
-    <div className="max-w-screen-xl flex flex-col  p-4 h-full mx-auto w-full">
-      <div className="text-xl sm:text-4xl mx-auto mb-4">
+    <div className="max-w-screen-xl flex flex-col p-4 h-full mx-auto w-full">
+      <div className="text-2xl sm:text-3xl md:text-4xl mx-auto mb-4">
         <p>Let&apos;s make something great together!</p>
       </div>
-      <div className="md:text-xl md:w-1/2 border border-purple-600 md:mx-auto">
-        <div className="flex justify-between w-1/2 mx-auto p-4 text-2xl sm:text-3xl">
-          <FontAwesomeIcon className='' icon={faLinkedin} />
-          <FontAwesomeIcon className='' icon={faGithub} />
+      <div className="md:text-xl sm:w-3/4 md:w-4/5 lg:w-1/2 border border-purple-600 sm:mx-auto">
+        <div className="flex justify-between w-1/2 mx-auto p-4 text-xl sm:text-2xl md:text-3xl ">
+          <a className='hover:text-purple-400 transition delay-75' href='https://www.linkedin.com/in/sean-morrison-2a7201217/'>
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a className='hover:text-purple-400 transition delay-75' href='https://github.com/sMorrison26/'>
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
         </div>
         <hr className='w-4/5 mx-auto'></hr>
         <div className='flex flex-col md:flex-row md:justify-between p-4 overflow-hidden'>
