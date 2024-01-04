31fe82177b85650e6915d97c7eb9ca81fa0317a4
sMorrison26 <morris10@rpi.edu>
2024-01-03 21:59:10 -0500

    Part 1 one about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,31 @@
 import Layout from '@/components/layout/layout.component'
+import onAHill from '@/images/onAHill.webp'
+import Image from 'next/image'
 import type { ReactElement } from 'react'
 import type { NextPageWithLayout } from './_app'
 
 const About: NextPageWithLayout = () => {
-  return <></>;
+  return (
+    <div className="max-w-screen-xl flex flex-col p-4 h-full mx-auto w-full">
+      <div id='pitch' className='flex'>
+        <figure className='h-full basis-1/5 mr-4'>
+          <Image src={onAHill} className='object-cover rounded-3xl shadow-2xl shadow-theme-1 h-full' alt='More Pictures of Sean' />
+        </figure>
+        <div className='basis-4/5'>
+          <p className='text-4xl mb-4'>Meet Sean.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus, sem id ultricies vestibulum, enim orci dictum erat, vel laoreet urna ante a mauris. Ut vulputate ex vel hendrerit tincidunt. Phasellus id tortor eleifend, fringilla purus nec, facilisis est. Integer consectetur tempor nunc, sit amet luctus orci condimentum at. In vitae viverra purus. Nam sollicitudin, risus vel pretium bibendum, lacus ipsum sodales diam, non ultrices odio turpis ut lacus. Nam pellentesque lectus venenatis ex faucibus, et facilisis turpis sollicitudin. Ut tincidunt lectus non posuere varius. Sed quis elit at orci iaculis scelerisque. Maecenas dictum tempor eleifend. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam nisi augue, sodales ultricies gravida at, ullamcorper sed purus.</p>
+
+        </div>
+      </div>
+      <div id='certs'>
+
+      </div>
+      <div id='resume'>
+
+      </div>
+    </div>
+  
+  );
 };
 
 About.getLayout = function getLayout(page: ReactElement) {
