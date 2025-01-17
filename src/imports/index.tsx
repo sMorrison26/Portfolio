3578ed8c8a45824fe3f2c0ae5f4b5b226b74f756
sMorrison26 <morris10@rpi.edu>
2025-01-16 22:34:53 -0500

    Add a technical skills section to home page

---
+++ src/imports/index.tsx
@@ -0,0 +1,58 @@
+import onAHill from '@/images/onAHill.webp'
+import pythonLogo from '@/images/Python.webp'
+import SkillLogo from '@/components/skillLogo/skillLogo.component'
+import cLogo from '@/images/C.webp'
+import cPlusPlusLogo from '@/images/C++.webp'
+import cSharpLogo from '@/images/Csharp.webp'
+import phpLogo from '@/images/PHP.webp'
+import SQLlogo from '@/images/SQL.webp'
+import JavaScriptLogo from '@/images/JavaScript.webp'
+import TypeScriptLogo from '@/images/TypeScript.webp'
+import HTMLLogo from '@/images/HTML.webp'
+import CSSLogo from '@/images/CSS.webp'
+import NodeJsLogo from '@/images/NodeJs.webp'
+import ReactJsLogo from '@/images/ReactJs.webp'
+import NextJsLogo from '@/images/nextjs.webp'
+import DotNetLogo from '@/images/DotNet.webp'
+import TailwindLogo from '@/images/tailwind-svgrepo-com.svg'
+import AngularJsLogo from '@/images/AngularJs.webp'
+import GitLogo from '@/images/git.webp'
+import LinuxLogo from '@/images/linux.webp'
+import AzureLogo from '@/images/MicrosoftAzure.webp'
+import AWSLogo from '@/images/aws.webp'
+import JavaLogo from '@/images/Java.webp'
+import BashLogo from '@/images/Bash.webp'
+import BootstrapLogo from '@/images/Bootstrap.webp'
+import ExpressLogo from '@/images/Expressjs.webp'
+import MongoDB from '@/images/MongoDB.webp'
+import headshot from '@/images/headshot_2.webp'
+
+export const Images = {
+  onAHill,
+  pythonLogo,
+  cLogo,
+  cPlusPlusLogo,
+  cSharpLogo,
+  phpLogo,
+  SQLlogo,
+  JavaScriptLogo,
+  TypeScriptLogo,
+  HTMLLogo,
+  CSSLogo,
+  NodeJsLogo,
+  ReactJsLogo,
+  NextJsLogo,
+  DotNetLogo,
+  TailwindLogo,
+  AngularJsLogo,
+  GitLogo,
+  LinuxLogo,
+  AzureLogo,
+  AWSLogo,
+  JavaLogo,
+  BashLogo,
+  BootstrapLogo,
+  ExpressLogo,
+  MongoDB,
+  headshot,
+}
