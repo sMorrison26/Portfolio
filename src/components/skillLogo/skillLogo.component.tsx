64d8940b93806851024f2bd8f186a3650e4d2018
sMorrison26 <morris10@rpi.edu>
2025-01-16 22:38:41 -0500

    Bug fix deployment error

--- src/components/skillLogo/skillLogo.component.tsx
+++ src/components/skillLogo/skillLogo.component.tsx
@@ -7,7 +7,7 @@ export default function SkillLogo({ src, alt, color }) {
 
   useEffect(() => {
     setOutlineColor('outline-' + color + '-700');
-  }, [outlineColor]);
+  }, [color]);
 
   return (
     <div className={`flex justify-center items-center outline outline-2 ${color} rounded-sm mx-auto p-2 w-10 sm:w-12 md:w-14 lg:w-18 h-10 sm:h-12 md:h-14 lg:h-18`}>
